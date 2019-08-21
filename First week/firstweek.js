38f4778845e1d587fcfd939a4197f431e3af8441
AsiaAlharby1 <53453766+AsiaAlharby1@users.noreply.github.com>
2019-08-21 22:20:40 +0300

    Add files via upload

---
+++ First week/firstweek.js
@@ -0,0 +1,19 @@
+
+// day 1
+// first code 
+document.write('مرحبا بكم في عالم الجافا سكريبت');
+
+document.write(5+6)+ '\n';
+
+/* use varibul + day 2,3 */
+
+var x=5;
+    y=7;
+document.write(" The sum is \t" + x+y);
+
+// day 4,5
+
+var num=10 +'\n';
+document.write("Hello, this is my first challenge and I will do it"+ num + "\ itmes/nI will do it, \nI will learn JavaScript");
+
+
