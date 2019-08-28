93b548ed5994860b5e55990ad11a340b29e7ab89
AsiaAlharby1 <53453766+AsiaAlharby1@users.noreply.github.com>
2019-08-29 02:46:29 +0300

    Create day6.js

---
+++ First week/day6.js
@@ -0,0 +1,13 @@
+var $num=9;
+document.write ( $num +'\n\t');
+
+function _sum(x,y){
+  document.write("the sum is\n\t "+ (x+y));
+}
+_sum(7,8);
+
+function fruit(){
+  fruit=["pear,waterlin,pinappple,strawbary \n\t"];
+  document.write( fruit);
+}
+fruit();
